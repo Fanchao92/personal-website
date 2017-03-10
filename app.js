0aa9d6cf8768898079bb79066a5ee46a409c612a
Fanchao Zhou <fanchaozhou1992@gmail.com>
2017-03-10 13:09:18 -0600

    About Me ready-no fading effect

--- app.js
+++ app.js
@@ -8,21 +8,37 @@ var app = express();
 app.set('view engine', 'hbs');
 app.engine('html', hbs.__express);
 app.use(express.static(`${__dirname}/public`));
+hbs.registerPartials(`${__dirname}/views/partials`);
 
 //Render the main page
 app.get('/', (req, res) => {
 	var data = {
 		title: 'Welcome to Fanchao\'s Page!',
+		navZoomIn: '150%',
+		pZoomIn: '120%',
+		signature: 'images/nav-header-brand.png',
+		zhuhaiImg: 'images/zhuhai.png',
+		rootURL: `${req.protocol}://${req.get('Host')}`,
+		whiteColor: '#ffffff',
+		blueColor: '#16e0cf',
 		maroonColor: '#800000',
-		whiteColor: '#FFFFFF',
-		zoomInSize: '150%',
-		signature: 'images/nav-header-brand.png'
+		fontStyle: 'italic'
 	};
 
 	res.render('home.html', data);
 });
 
 ////////////   All the APIs available in my app   ////////////////
+app.get('/about', (req, res) => {
+	//return a JSON object containing a brief introduction and a personal image
+	var aboutObj = {};
+
+	aboutObj.myImg = "images/my-picture.jpg"
+	aboutObj.selfIntro = fs.readFileSync(`public/text/self-intro.txt`, {encoding: 'utf8'});
+
+	res.send(aboutObj);
+});
+
 app.get('/edu', (req, res) => {
 	//return a JSON array containing my education
 });
@@ -37,10 +53,6 @@ app.get('/professional/skills', (req, res) => {
 
 app.get('/professional/projects', (req, res) => {
 	//return a JSON array containing all the projects I've done
-});
-
-app.get('/professional', (req, res) => {
-	//return all the information about my career in one JSON object
 });
 //////////////////// End of the API part ////////////////////////
 
